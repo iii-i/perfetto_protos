35d7a113b36ee14dd3c5db03761ca3aeb567d255
Ilya Leoshkevich <iii@linux.ibm.com>
2026-04-07 21:10:33 +0000

    protozero: LE-encode fixed32/64 in Message::AppendFixed
    Message::AppendFixed currently memcpy's the value raw, causing
    fixed32/64 fields serialized on BE hosts to be wrong. Fix by
    LE-encoding before memcpy, and drop the "Unimplemented for big endian
    archs" #error in message.cc.

--- include/perfetto/protozero/message.h
+++ include/perfetto/protozero/message.h
@@ -26,6 +26,7 @@
 
 #include "perfetto/base/export.h"
 #include "perfetto/base/logging.h"
+#include "perfetto/ext/base/endian.h"
 #include "perfetto/protozero/contiguous_memory_range.h"
 #include "perfetto/protozero/proto_utils.h"
 #include "perfetto/protozero/scattered_stream_writer.h"
@@ -135,7 +136,8 @@ class PERFETTO_EXPORT_COMPONENT Message {
     uint8_t* pos = buffer;
 
     pos = proto_utils::WriteVarInt(proto_utils::MakeTagFixed<T>(field_id), pos);
-    memcpy(pos, &value, sizeof(T));
+    T wire_value = perfetto::base::HostToLE(value);
+    memcpy(pos, &wire_value, sizeof(T));
     pos += sizeof(T);
     // TODO: Optimize memcpy performance, see http://crbug.com/624311 .
     WriteToStream(buffer, pos);
